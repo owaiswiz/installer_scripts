d0f7b34a6d9413f1fe2a6867864bc6e013cc48e4
owaiswiz <owaiswizard@gmail.com>
2016-08-14 01:52:30 +0530

    Changes to make MySQL password Prompt Dissappear

--- Ubuntu-14.04/WordPress/WordPressWithLEMP.sh
+++ Ubuntu-14.04/WordPress/WordPressWithLEMP.sh
@@ -19,8 +19,6 @@ apt-get update;
 apt-get -y upgrade;
 # Install Nginx/MySQL
 apt-get -y install debconf-utils
-echo mysql-server mysql-server/root_password password  | sudo debconf-set-selections
-echo mysql-server mysql-server/root_password_again password  | sudo debconf-set-selections
 sudo apt-get -y install mysql-server
 sudo apt-get install -y php5-fpm php5-mysql mysql-client unzip;
 echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
