4469b00cd5cd61df0c33263311ac7dc4033b4fb7
Owais Wiz <me@owaiswiz.com>
2016-08-25 23:49:21 +0530

    Add Script for LEMP Stack on CentOS 7

---
+++ CentOS-7/Stacks/LEMP.sh
@@ -0,0 +1,51 @@
+yum -y install epel-release
+yum -y install nginx php-fpm php-mysql mariadb-server mariadb
+
+mkdir -p /var/www/html
+cp /usr/share/nginx/html/index.html /var/www/html/
+
+sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php.ini
+sed -i -e "s|listen = 127.0.0.1:9000|listen = /var/run/php-fpm/php-fpm.sock|" /etc/php-fpm.d/www.conf
+sed -i -e "s/default_server//" /etc/nginx/nginx.conf
+
+echo 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server ipv6only=on;
+        root /var/www/html;
+        index index.php index.html index.htm;
+        server_name localhost;
+        location / {
+            # First attempt to serve request as file, then
+            # as directory, then fall back to displaying a 404.
+            try_files $uri $uri/ =404;
+            # Uncomment to enable naxsi on this location
+            # include /etc/nginx/naxsi.rules
+        }
+        error_page 404 /404.html;
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+            root /usr/share/nginx/html;
+        }
+        location ~ \.php$ {
+            try_files $uri =404;
+            fastcgi_split_path_info ^(.+\.php)(/.+)$;
+            fastcgi_pass unix:/var/run/php-fpm/php-fpm.sock;
+            fastcgi_index index.php;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            include fastcgi_params;
+        }
+      }' > /etc/nginx/conf.d/default.conf
+
+echo '<?php
+      phpinfo();
+      ?>' > /var/www/html/info.php
+
+	  
+MYSQLPASS=`dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev`
+mysqladmin -u root -h localhost password "$MYSQLPASS"
+echo -e $MYSQLPASS > /root/mysqlpass.txt
+
+systemctl start php-fpm
+systemctl enable php-fpm.service
+systemctl enable nginx.service
+systemctl restart nginx
--- CentOS-7/Stacks/README.md
+++ CentOS-7/Stacks/README.md
@@ -3,6 +3,12 @@ Stacks
 
 LAMP.sh
 --------
-Shell Script that Installs a basic ["LAMP" stack on CentOS 7](https://lideploy.com/tutorials/how-to-install-lamp-stack-on-centos-7/) with Apache, PHP, and MariaDB.
+Shell Script that Automatically Installs a basic ["LAMP" stack on CentOS 7](https://lideploy.com/tutorials/how-to-install-lamp-stack-on-centos-7/) with Apache, PHP, and MariaDB.
 
-Tutorial to use this script - [How to install LAMP Stack automatically on CentOS7 with just 3 clicks](https://lideploy.com/tutorials/setup-lamp-stack-automatically-on-centos-7/)
+Tutorial to use this script - [How to install LAMP Stack automatically on CentOS 7 with just 3 clicks](https://lideploy.com/tutorials/setup-lamp-stack-automatically-on-centos-7/)
+
+LEMP.sh
+--------
+Shell Script that Automatically Installs a basic ["LEMP" stack on CentOS 7](https://lideploy.com/tutorials/how-to-install-and-setup-lemp-stack-on-centos-7/) with Nginx, PHP, and MariaDB.
+
+Tutorial to use this script - [How to install LEMP Stack automatically on CentOS 7 with just 3 clicks](https://lideploy.com/tutorials/how-to-setup-lemp-stack-automatically-on-centos-7-with-just-3-clicks/)
