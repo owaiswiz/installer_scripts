a7ca525da99b0dd825cc4677e8f003f489bfb294
owaiswiz <owaiswizard@gmail.com>
2016-08-28 01:20:45 +0530

    Start MariaDB before changing password

--- CentOS-7/Stacks/LAMP.sh
+++ CentOS-7/Stacks/LAMP.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 yum install -y httpd mariadb-server mariadb php php-mysql
 sudo bash -c 'echo -e "<?php\nphpinfo();\n?>" > /var/www/html/info.php'
-MYSQLPASS=`dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev`
-mysqladmin -u root -h localhost password "$MYSQLPASS"
-echo -e $MYSQLPASS > /root/mysqlpass.txt
 systemctl start httpd.service
 systemctl enable httpd.service
 systemctl start mariadb
 systemctl enable mariadb.service
+MYSQLPASS=`dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev`
+mysqladmin -u root -h localhost password "$MYSQLPASS"
+echo -e $MYSQLPASS > /root/mysqlpass.txt
