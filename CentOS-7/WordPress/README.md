44d8a3572ed7399bdf022104f9e949af2fc0a234
Owais Wiz <me@owaiswiz.com>
2016-09-05 01:13:10 +0530

    Add Script for WordPress with LAMP stack on CentOS 7

---
+++ CentOS-7/WordPress/README.md
@@ -0,0 +1,10 @@
+WordPress
+===========
+
+
+WordPressWithLAMP.sh
+---------------
+
+Shell Script that Installs a basic ["LAMP" stack](https://lideploy.com/tutorials/how-to-install-lamp-stack-on-centos-7/) with Apache, PHP, and MariaDB along with [WordPress on LAMP Stack](https://lideploy.com/tutorials/how-to-install-wordpress-with-lamp-stack-apache-on-centos-7/)
+
+Tutorial to use this script - [Install WordPress automatically on Ubuntu 14.04 with Apache/LAMP Stack](https://lideploy.com/tutorials/setup-wordpress-automatically-with-apache-or-lamp-stack-on-centos-7)
---
+++ CentOS-7/WordPress/WordPressWithLAMP.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# WordPress Setup Script
+#
+# This script will install and configure Wordpress on
+# an CentOS 7 droplet
+
+# Generate root and wordPress mysql passwords
+rootmysqlpass=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
+wpmysqlpass=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev | tr -dc 'a-zA-Z0-9'`;
+
+# Write passwords to file
+echo "Root MySQL Password: $rootmysqlpass" > /root/passwords.txt;
+echo "Wordpress MySQL Password: $wpmysqlpass" >> /root/passwords.txt;
+
+# Update CentOS
+yum -y update;
+
+# Install Apache/MySQL
+yum -y install httpd php php-mysql mariadb-server mariadb unzip;
+
+# Start services
+systemctl start mariadb;
+systemctl start httpd;
+
+# Download and uncompress WordPress
+wget https://wordpress.org/latest.zip -O /tmp/wordpress.zip;
+cd /tmp/;
+unzip /tmp/wordpress.zip;
+# Set up database user
+/usr/bin/mysqladmin -u root -h localhost create wordpress;
+/usr/bin/mysqladmin -u root -h localhost password $rootmysqlpass;
+/usr/bin/mysql -uroot -p$rootmysqlpass -e "CREATE USER wordpress@localhost IDENTIFIED BY '"$wpmysqlpass"'";
+/usr/bin/mysql -uroot -p$rootmysqlpass -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@localhost";
+
+# Configure WordPress
+cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php;
+sed -i "s/'DB_NAME', 'database_name_here'/'DB_NAME', 'wordpress'/g" /tmp/wordpress/wp-config.php;
+sed -i "s/'DB_USER', 'username_here'/'DB_USER', 'wordpress'/g" /tmp/wordpress/wp-config.php;
+sed -i "s/'DB_PASSWORD', 'password_here'/'DB_PASSWORD', '$wpmysqlpass'/g" /tmp/wordpress/wp-config.php;
+
+for i in `seq 1 8`
+do
+wp_salt=$(</dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()\-_ []{}<>~`+=,.;:/?|' | head -c 64 | sed -e 's/[\/&]/\\&/g');
+sed -i "0,/put your unique phrase here/s/put your unique phrase here/$wp_salt/" /tmp/wordpress/wp-config.php;
+done
+
+cp -Rf /tmp/wordpress/* /var/www/html/.;
+rm -f /var/www/html/index.html;
+chown -Rf apache:apache /var/www/html;
+systemctl enable httpd.service;
+systemctl enable mariadb.service;
+systemctl restart httpd.service;
